e0c1ee0bec8ff2d07c66ce9c58b52ad6b9eebb4e
HSN <hasanlock@gmail.com>
2023-10-14 15:21:55 +0600

    feat: add all utils to json builder type

--- src/utils.go
+++ src/utils.go
@@ -36,33 +36,33 @@ func (jdb *JsonDataBuilder) Export() string {
 }
 
 // process and return JSON data
-func getIni(data, query, defaultVal string) string {
-	iData := ini.New().Load([]byte(data))
+func (jdb *JsonDataBuilder) getIni() string {
+	iData := ini.New().Load([]byte(jdb.feature.Data))
 
-	res := gjson.Get(string(iData.Marshal2Json()), query)
+	res := gjson.Get(string(iData.Marshal2Json()), jdb.feature.Query)
 
 	if !res.Exists() {
-		return defaultVal
+		return jdb.feature.DefaultVal
 	}
 
 	return res.String()
 }
 
 // process and return JSON data
-func getJson(data, query, defaultVal string) string {
-	res := gjson.Get(data, query)
+func (jdb *JsonDataBuilder) getJson() string {
+	res := gjson.Get(jdb.feature.Data, jdb.feature.Query)
 
 	if !res.Exists() {
-		return defaultVal
+		return jdb.feature.DefaultVal
 	}
 
 	return res.String()
 }
 
-func getToml(data, query, defaultVal string) string {
+func (jdb *JsonDataBuilder) getToml() string {
 
 	var jsonI interface{}
-	dataB := []byte(data)
+	dataB := []byte(jdb.feature.Data)
 
 	if err := toml.Unmarshal(dataB, &jsonI); err != nil {
 		stop(fmt.Sprintf("error: TOML conversion error. %s", err.Error()))
@@ -74,48 +74,46 @@ func getToml(data, query, defaultVal string) string {
 	}
 
 	jsonS := string(jsonBytes)
-	res := gjson.Get(jsonS, query)
+	res := gjson.Get(jsonS, jdb.feature.Query)
 
 	if !res.Exists() {
-		return defaultVal
+		return jdb.feature.DefaultVal
 	}
 
 	return res.String()
-
 }
 
 // process and return XML data
-func getXml(data, query, defaultVal string) string {
-	xml := strings.NewReader(data)
+func (jdb *JsonDataBuilder) getXml() string {
+	xml := strings.NewReader(jdb.feature.Data)
 	jsonData, err := xmlToJson.Convert(xml)
 
 	if err != nil {
 		stop(fmt.Sprintf("error: XML conversion error. %s", err.Error()))
 	}
 
-	res := gjson.Get(jsonData.String(), query)
+	res := gjson.Get(jsonData.String(), jdb.feature.Query)
 
 	if !res.Exists() {
-		return defaultVal
+		return jdb.feature.DefaultVal
 	}
 
 	return res.String()
 }
 
 // process and return YAML data
-func getYaml(data, query, defaultVal string) string {
-	jsonData, err := yaml.YAMLToJSON([]byte(data))
+func (jdb *JsonDataBuilder) getYaml() string {
+	jsonData, err := yaml.YAMLToJSON([]byte(jdb.feature.Data))
 
 	if err != nil {
 		stop(fmt.Sprintf("error: YAML conversion error. %s", err.Error()))
 	}
 
-	res := gjson.Get(string(jsonData), query)
+	res := gjson.Get(string(jsonData), jdb.feature.Query)
 
 	if !res.Exists() {
-		return defaultVal
+		return jdb.feature.DefaultVal
 	}
 
 	return res.String()
-
 }
